efa9dd50dd07f237f77013f22b3635cc8e537611
fabriik <david+fabriik@nordicloop.com>
2021-11-24 09:25:03 -0400

    adding us states

---
+++ lib/src/res/country_codes_us_states.dart
@@ -0,0 +1,28 @@
+final List<Map<String, dynamic>> countryCodes = [
+  {
+    "e164_cc": "1",
+    "iso2_cc": "US-AL",
+    "e164_sc": 0,
+    "geographic": true,
+    "level": 1,
+    "name": "Alabama",
+    "example": "2012345678",
+    "display_name": "Alabama (US-AL) [+1]",
+    "full_example_with_plus_sign": "+12012345678",
+    "display_name_no_e164_cc": "Alabama (US-AL)",
+    "e164_key": "1-US-0"
+  },
+  {
+    "e164_cc": "1",
+    "iso2_cc": "US-NY",
+    "e164_sc": 0,
+    "geographic": true,
+    "level": 1,
+    "name": "United States (New York)",
+    "example": "2012345678",
+    "display_name": "United States (New York) (US-NY) [+1]",
+    "full_example_with_plus_sign": "+12012345678",
+    "display_name_no_e164_cc": "United States (New York) (US-NY)",
+    "e164_key": "1-US-0"
+  }
+];
